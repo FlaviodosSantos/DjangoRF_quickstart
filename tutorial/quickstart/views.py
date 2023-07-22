184e1d17850077f4aaccdbb5c7f0d55e9544af2e
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-07-22 15:56:12 -0300

    mod quickstart/views.py

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    Endpoint da API que permite que os usuários sejam visualizados ou editados.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    Endpoint da API que permite que os grupos sejam visualizados ou editados.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    
